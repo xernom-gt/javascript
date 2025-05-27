791c98ae3123b4248c382ab13f2d3e5800fa7a1e
xernom-gt <dasboard10@gmail.com>
2025-05-27 11:00:22 +0700

    celcius to fahrenheit

--- soal 7/script.js
+++ soal 7/script.js
@@ -1,6 +1,6 @@
 function convertToFahrenheit() {
     const celcius = Number(document.getElementById('celcius').value)
     let AF = (celcius * 9/5) + 32
-    document.getElementById('hasil').innerText = AF
+    document.getElementById('hasil').innerText = AF + 'F'
     return AF
 }
