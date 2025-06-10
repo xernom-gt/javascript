90d9fe405c8c7a0ebb5f548064531c940458eac6
xernom-gt <dasboard10@gmail.com>
2025-06-10 21:25:40 +0700

    lamp

--- soal 13/script.js
+++ soal 13/script.js
@@ -1,6 +1,9 @@
 const ligth = document.querySelector('.light-bulb');
 
-const switchButton = document.getElementById('Switch');
-switchButton.addEventListener('click',() => {
+const switcher = document.querySelector('Switch');
+const active = switcher.classList.toggle('active')
+ligth.appendChild(active)
+switcher.addEventListener('click',() => {
     ligth.classList.toggle('on')
+    ligth.classList.toggle('active')
 })
